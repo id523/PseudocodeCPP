7127de49c392f954e0d9f9cda6928906b7f41934
id523 <edward.giles@dulwich-suzhou.cn>
2017-04-03 09:05:56 +0800

    Started implementing prefix tree for storing object members

--- PseudocodeCPP/ValueTree.cpp
+++ PseudocodeCPP/ValueTree.cpp
@@ -1,12 +1,56 @@
 #include "stdafx.h"
 #include "ValueTree.h"
 
-
-ValueTree::ValueTree()
-{
+template<class T>
+ValueTree<T>::ValueTree() {
 }
 
+template<class T>
+ValueTree<T>::~ValueTree() {
+	for (int i = 0; i < 16; i++) {
+		if (ChildNodes[i]) delete ChildNodes[i];
+	}
+}
 
-ValueTree::~ValueTree()
-{
+template<class T>
+ValueTree<T>* ValueTree<T>::Subtree(byte key, bool add) {
+	int high = (key >> 4) & 0x0F;
+	int low = key & 0x0F;
+	ValueTree<T>* highNode = ChildNodes[high];
+	if (!highNode) {
+		if (add) {
+			highNode = new ValueTree<T>();
+			ChildNodes[high] = highNode;
+		} else {
+			return nullptr;
+		}
+	}
+	ValueTree<T>* lowNode = highNode->ChildNodes[low];
+	if (add && !lowNode) {
+		lowNode = new ValueTree<T>();
+		highNode->ChildNodes[low] = lowNode;
+	}
+	return lowNode;
+}
+
+template<class T>
+ValueTree<T>* ValueTree<T>::Subtree(byte key) const {
+	return Subtree(key, false);
+}
+
+template<class T>
+bool ValueTree<T>::DeleteSubtree(byte key) {
+	int high = (key >> 4) & 0x0F;
+	int low = key & 0x0F;
+	ValueTree<T>* highNode = ChildNodes[high];
+	if (!highNode) {
+		return false;
+	}
+	ValueTree<T>*& lowNode = highNode->ChildNodes[low];
+	if (lowNode) {
+		delete lowNode;
+		lowNode = nullptr;
+	} else {
+		return false;
+	}
 }
--- PseudocodeCPP/ValueTree.h
+++ PseudocodeCPP/ValueTree.h
@@ -1,10 +1,15 @@
 #pragma once
-class ValueTree
-{
+#include "Byte.h"
+template<class T>
+class ValueTree {
 private:
 	ValueTree* ChildNodes[16];
 public:
 	ValueTree();
 	~ValueTree();
+	ValueTree* Subtree(byte key, bool add);
+	ValueTree* Subtree(byte key) const;
+	bool DeleteSubtree(byte key);
+	// TODO: Access to optional value of type T
 };
 
